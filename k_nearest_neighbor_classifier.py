0c5b83ac94c9d01813e51fbbdcc5bf539b369317
calt-laboratory <christophalt@web.de>
2021-04-22 21:46:50 +0200

    add knn classifier file and knn test file

---
+++ k_nearest_neighbor_classifier.py
@@ -0,0 +1,42 @@
+import numpy as np
+from collections import Counter
+
+
+class KnnClassifier():
+
+    def __init__(self, k: int=3):
+        """Constructor to initialize the number of the nearest neighbors."""
+        self.k = k
+
+    def _euclidean_distance(self, p: np.ndarray, q: np.ndarray):
+        """Computation of the Euclidean distance between to data points."""
+        return np.linalg.norm(p - q)
+
+    def _k_nearest_neighbors(self, X_new: np.ndarray):
+        """Computation of the k-nearest neighbors of one or more sample x-data."""
+        knn_idx_array = np.array([np.argsort([self._euclidean_distance(p_i, q_i) for q_i in self.X])[:self.k] for p_i in X_new])
+        return knn_idx_array
+
+    def _class_majority_voting(self, knn_idx: np.ndarray):
+        """Choose the class for one or more data points by taking this class which occured most
+         during the k-nearest neighbor computation (= majority voting)."""
+        class_labels_list = [self.y[i] for i in knn_idx]
+        major_class = Counter(class_labels_list).most_common(1)
+        return major_class[0][0]
+
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        """Initializing X-data and y-data"""
+        self.X = X
+        self.y = y
+
+    def predict(self, X_new: np.ndarray):
+        """Predict the class for one or more new data points."""
+        knn_idx_array = self._k_nearest_neighbors(X_new)
+        class_predictions = np.array([self._class_majority_voting(i) for i in knn_idx_array])
+        return class_predictions
+
+    def accuracy(self, y_true: np.ndarray, y_predictions: np.ndarray):
+        """Computation of the accuracy of the model."""
+        acc = np.sum([y_true_i == y_pred_i for y_true_i, y_pred_i in zip(y_true, y_predictions)]) / len(y_true)
+        return acc
+
---
+++ knn_test_file.py
@@ -0,0 +1,21 @@
+# import libraries
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+from k_nearest_neighbor_classifier import *
+
+# build Iris data set
+iris_dataset = datasets.load_iris()
+X, y = iris_dataset.data, iris_dataset.target
+
+# split dataset into train set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=37)
+
+# use knn classifier
+clf = KnnClassifier(k=5)
+clf.fit(X_train, y_train)
+preds = clf.predict(X_test)
+print(f"True classes: {y_test}")
+print(f"Predicted classes: {preds}")
+print(f"Accuracy: {clf.accuracy(y_test, preds)}")
